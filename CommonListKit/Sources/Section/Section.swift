be5d62adf376f30e3f8bfe662f67eccd83267639
박기석 <gkfnrkfl@naver.com>
2024-11-11 22:20:07 +0900

    style: Section 설명 주석

--- CommonListKit/Sources/Section/Section.swift
+++ CommonListKit/Sources/Section/Section.swift
@@ -7,15 +7,22 @@
 
 import Foundation
 
+/**
+ List 섹션  모델
+ */
 struct Section: Hashable {
+    /// Section 고유 ID 값
     let identifier: String
     
+    /// 아이템 사이 Line 간격
     let minimumLineSpacing: CGFloat
+    /// 아이템과 아이템 간격
     let minimumInteritemSpacing: CGFloat
-    
+    /// 섹션 Header Component
     let header: (any Component)?
+    /// 섹션 Footer Component
     let footer: (any Component)?
-    
+    /// 섹션 아이템 Component 배열
     let items: [any Component]
     
     func hash(into hasher: inout Hasher) {

bd9412ac3cd2c0f4431c0298cc10b5139b6920a7
박기석 <gkfnrkfl@naver.com>
2024-11-11 15:45:35 +0900

    style: Component에 설명 주석 추가

--- CommonListKit/Sources/Component/Component.swift
+++ CommonListKit/Sources/Component/Component.swift
@@ -7,10 +7,36 @@
 
 import UIKit
 
+/**
+ List 아이템 모델 Protocol
+ 
+ List에 아이템 뷰에 데이터 바인딩을 하기 위한 모델 설정 및 뷰 렌더링 동작 설정
+ 
+ 사용 예제
+ ```swift
+ struct TestComponent: Component {
+    typealias Content = TestView
+ 
+    let identifier = "test_component"
+ 
+    func render(content: Content) {
+        // content view render
+    }
+ }
+ ```
+ */
 protocol Component: Hashable {
+    /// Component에 매칭되는 뷰 지정
     associatedtype Content: UIView
     
+    /// Component 고유 ID 값 설정
     var identifier: String { get }
     
+    /**
+     `'Contnet'` View에 데이터 적용하는 함수
+     
+     - Parameters:
+        - content: Component에 매칭되어 데이터를 적용할 Content View
+     */
     func render(content: Content)
 }

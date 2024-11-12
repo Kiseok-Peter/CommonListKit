6df0f4cdbbc6379e5d0891e16e461405204c0c8e
박기석 <gkfnrkfl@naver.com>
2024-11-12 17:23:36 +0900

    test: ComponentTests, SectionTests 테스트 개선

--- CommonListKitTests/Sources/Section/SectionTests.swift
+++ CommonListKitTests/Sources/Section/SectionTests.swift
@@ -11,18 +11,20 @@ import Nimble
 
 final class SectionTests: QuickSpec {
     override class func spec() {
-        let section1 = Section(identifier: "section_1", items: [DummyComponent(identifier: "dummy_component")])
+        var section1: Section!
         var section2: Section!
         
         describe("Section 판별") {
             context("동일성 검사") {
                 beforeEach {
+                    section1 = Section(identifier: "section_1", items: [DummyComponent(identifier: "dummy_component")])
                     section2 = Section(identifier: "section_1", items: [DummyComponent(identifier: "dummy_component")])
                 }
                 
                 it("같은 경우") {
                     expect(section1).to(equal(section2))
                     expect(section1.identifier).to(equal(section2.identifier))
+                    expect(section1.hashValue).to(equal(section2.hashValue))
                 }
                 
                 afterEach {
@@ -32,12 +34,14 @@ final class SectionTests: QuickSpec {
             
             context("identifier 검사") {
                 beforeEach {
+                    section1 = Section(identifier: "section_1", items: [DummyComponent(identifier: "dummy_component")])
                     section2 = Section(identifier: "section_2", items: [DummyComponent(identifier: "dummy_component")])
                 }
                 
                 it("다른 경우") {
                     expect(section1).toNot(equal(section2))
                     expect(section1.identifier).toNot(equal(section2.identifier))
+                    expect(section1.hashValue).toNot(equal(section2.hashValue))
                 }
                 
                 afterEach {
@@ -47,11 +51,13 @@ final class SectionTests: QuickSpec {
             
             context("items 검사") {
                 beforeEach {
+                    section1 = Section(identifier: "section_1", items: [DummyComponent(identifier: "dummy_component")])
                     section2 = Section(identifier: "section_1", items: [DummyComponent(identifier: "dummy_component_1")])
                 }
                 
                 it("다른 경우") {
                     expect(section1).toNot(equal(section2))
+                    expect(section1.hashValue).toNot(equal(section2.hashValue))
                 }
                 
                 afterEach {

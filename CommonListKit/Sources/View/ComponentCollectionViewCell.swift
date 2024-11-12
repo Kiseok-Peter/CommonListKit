ea780a6e29b785e19aa2f66345dbdbb7b73df313
박기석 <gkfnrkfl@naver.com>
2024-11-12 22:41:04 +0900

    feat: ComponentCollectionViewCell Component용 CollectionViewCell 추가

---
+++ CommonListKit/Sources/View/ComponentCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ComponentCollectionViewCell.swift
+//  CommonListKit
+//
+//  Created by 박기석 on 11/12/24.
+//
+
+import UIKit
+
+final class ComponentCollectionViewCell: UICollectionViewCell, ComponentRenderable {
+    var content: UIView?
+}

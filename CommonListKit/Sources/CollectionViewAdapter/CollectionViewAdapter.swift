094df073417921b9422fad3f9f95d3fa600aecae
박기석 <gkfnrkfl@naver.com>
2024-11-13 00:11:29 +0900

    feat: Section 모델 파라미터 및 update 동작 추가

--- CommonListKit/Sources/CollectionViewAdapter/CollectionViewAdapter.swift
+++ CommonListKit/Sources/CollectionViewAdapter/CollectionViewAdapter.swift
@@ -7,10 +7,33 @@
 
 import UIKit
 
+
 public final class CollectionViewAdapter {
+    /// 처리할 UICollectionView
     weak var collectionView: UICollectionView?
+    
+    /// CollectionView에 적용된 Section 모델
+    var sections: [Section] = []
+    /// Section 적용 전용
+    var inputSections: [Section] {
+        get { sections }
+        set { updateSections(with: newValue) }
+    }
     
     public init(with collectionView: UICollectionView) {
         self.collectionView = collectionView
+    }
+}
+
+extension CollectionViewAdapter {
+    private func updateSections(with inputSections: [Section]) {
+        guard let collectionView else { return }
+        
+        Task {
+            await MainActor.run {
+                sections = inputSections
+                collectionView.reloadData()
+            }
+        }
     }
 }

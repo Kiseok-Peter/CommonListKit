28e61187d9896d4bd590a4f0022df9dfa46c1abb
박기석 <gkfnrkfl@naver.com>
2024-11-12 17:42:11 +0900

    feat: ComponentRenderable에 render 처리 추가

--- CommonListKit/Sources/View/ComponentRederable.swift
+++ CommonListKit/Sources/View/ComponentRederable.swift
@@ -13,7 +13,30 @@ import UIKit
 protocol ComponentRenderable: AnyObject {
     /// Render할 View
     var content: UIView? { get set }
+    /// content 뷰를 적용할 container 뷰
+    var container: UIView { get }
     
     /// Component에 View를 Render 하는 동작 함수
-    func render(component: some Component)
+    func render(component: ComponentWrapper)
+}
+
+extension ComponentRenderable where Self: UIView  {
+    func render(component: ComponentWrapper) {
+        if let content {
+            component.render(content: content)
+        } else {
+            let content = component.renderContent()
+            
+            container.addSubview(content)
+            content.translatesAutoresizingMaskIntoConstraints = false
+            content.topAnchor.constraint(equalTo: container.topAnchor).isActive = true
+            content.leadingAnchor.constraint(equalTo: container.leadingAnchor).isActive = true
+            content.trailingAnchor.constraint(equalTo: container.trailingAnchor).isActive = true
+            content.bottomAnchor.constraint(equalTo: container.bottomAnchor).isActive = true
+            
+            self.content = content
+            
+            render(component: component)
+        }
+    }
 }
